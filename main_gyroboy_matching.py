5e80b9a9b8db37e4efcbc762d78004202eef1b46
michaelarfreed <michaelarfreed@gmail.com>
2021-12-09 14:54:24 -0500

    initial commit of all 6 configurations/executable files

---
+++ main_gyroboy_matching.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env pybricks-micropython
+from main import *
+
+if __name__ == '__main__':
+    experiment = Experiment(form=1, behavior=True)
+    finished = experiment.run()
+    experiment.write_sensor_input_to_file()
